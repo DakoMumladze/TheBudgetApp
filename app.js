d536662a9a5cce814aee06715251007ecf664e79
Dako Mumladze <dmumladze@corp.europe-bet.com>
2019-05-06 14:37:23 +0400

    Budget controller setup

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
 
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var data = {
+        allItems:{
+            allExpenses: [],
+            allIncomes: [],
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 })();
 
 
 // UI CONTROLLER
 var UIController = (function(){
-
+    var DOMstrings = {
+        inputType:'.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn:'.add__btn'
+    }
     return{
         getinput: function(){
             return{
-                type: document.querySelector('.add__type').value,//Will be either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,//Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
         }
     }
 
@@ -24,6 +52,18 @@ var UIController = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
 
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+        document.addEventListener('keypress',function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    }
+    
     var ctrlAddItem = function(){
         //1.Get the field input data
         var input = UICtrl.getinput();
@@ -35,13 +75,14 @@ var controller = (function(budgetCtrl,UICtrl){
         //4.Calculate the budget
 
         //5.Display the budget update to UI
-    }
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
-
-    document.addEventListener('keypress',function(event){
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    };
+    return {
+        init: function(){
+            console.log('Application has started');
+            setupEventListeners();
         }
-    });
+    }
 
 })(budgetController,UIController);
+
+controller.init();
